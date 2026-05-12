dcc8ef72d8050bac773d011e5a0a6e1d09c9aaa7
WebScraping.AI <hello@webscraping.ai>
2026-05-12 12:47:09 +0700

    3.2.1

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -101,7 +101,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'OpenAPI-Generator/3.2.0/PHP';
+    protected $userAgent = 'OpenAPI-Generator/3.2.1/PHP';
 
     /**
      * Debug switch (default set to false)
@@ -491,7 +491,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    The version of the OpenAPI document: 3.2.1' . PHP_EOL;
-        $report .= '    SDK Package Version: 3.2.0' . PHP_EOL;
+        $report .= '    SDK Package Version: 3.2.1' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
